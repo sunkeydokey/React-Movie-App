f3504ea359d64363add4423b5871aefb45779f29
sunkeydokey <sunkeydokey@gmail.com>
2023-03-12 22:38:14 +0900

    Category 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import styled from 'styled-components';
 
 import Nav from './components/Nav';
 import Banner from './components/Banner';
+import Category from './components/Category';
 
 function App() {
   return (
     <Container>
       <Nav />
       <Banner />
+      <Category />
     </Container>
   );
 }
